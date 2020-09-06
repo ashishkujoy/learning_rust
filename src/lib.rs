eedbb8a96bce82269f141e7c2210d29433da8631
Ashish Kumar <ashishku@thoughtworks.com>
2020-09-06 20:41:40 +0530

    Replaces credit and debit with perform transaction

--- src/lib.rs
+++ src/lib.rs
@@ -32,18 +32,21 @@ impl Account {
         Account { account_number, balance: Cell::new(0) }
     }
 
-    pub fn credit(&self, transaction: Transaction) {
+    fn perform_transaction(&self, transaction: Transaction) -> Result<(), String> {
         let current_balance = self.balance.get();
-        self.balance.set(current_balance + transaction.amount)
-    }
-
-    pub fn debit(&self, transaction: Transaction) -> Result<(), String> {
-        let current_balance = self.balance.get();
-        if current_balance >= transaction.amount {
-            self.balance.set(current_balance - transaction.amount);
-            Ok(())
-        } else {
-            Err("Insufficient balance".to_string())
+        match transaction.transaction_type {
+            TransactionType::CashDeposit => {
+                self.balance.set(current_balance + transaction.amount);
+                Ok(())
+            }
+            TransactionType::CashWithdrawal => {
+                if current_balance >= transaction.amount {
+                    self.balance.set(current_balance - transaction.amount);
+                    Ok(())
+                } else {
+                    Err("Insufficient balance".to_string())
+                }
+            }
         }
     }
 
@@ -59,23 +62,23 @@ mod tests {
     #[test]
     fn should_credit_in_account() {
         let account = Account::new("abcd1234".to_string());
-        account.credit(Transaction::credit_of_amount(1000.45 as usize));
+        account.perform_transaction(Transaction::credit_of_amount(1000.45 as usize)).unwrap();
         assert_eq!(account.get_balance(), 1000.45 as usize)
     }
 
     #[test]
     fn should_debit_from_account() {
         let account = Account::new("abcd1234".to_string());
-        account.credit(Transaction::credit_of_amount(1000.45 as usize));
-        account.debit(Transaction::debit_of_amount(100 as usize)).unwrap();
+        account.perform_transaction(Transaction::credit_of_amount(1000.45 as usize)).unwrap();
+        account.perform_transaction(Transaction::debit_of_amount(100 as usize)).unwrap();
         assert_eq!(account.get_balance(), 900.45 as usize)
     }
 
     #[test]
     fn should_give_error_for_debit_on_insufficient_balance() {
         let account = Account::new("abcd1234".to_string());
-        account.credit(Transaction::credit_of_amount(1000.45 as usize));
-        let error = account.debit(Transaction::debit_of_amount(2000 as usize));
+        account.perform_transaction(Transaction::credit_of_amount(1000.45 as usize)).unwrap();
+        let error = account.perform_transaction(Transaction::debit_of_amount(2000 as usize));
 
         assert_eq!(error, Err("Insufficient balance".to_string()));
     }
