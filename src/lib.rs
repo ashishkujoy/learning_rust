48a0b130716bcfd59ede05d6c27461a1df0a988a
Ashish Kumar <ashishku@thoughtworks.com>
2020-09-06 19:25:54 +0530

    Adds transaction

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,46 @@
 use std::cell::Cell;
 
 struct Account {
-    balance: Cell<usize>
+    account_number: String,
+    balance: Cell<usize>,
 }
 
 unsafe impl Sync for Account {}
 
+struct Transaction {
+    amount: usize,
+    transaction_type: TransactionType,
+}
+
+enum TransactionType {
+    CashDeposit,
+    CashWithdrawal,
+}
+
+impl Transaction {
+    fn credit_of_amount(amount: usize) -> Self {
+        Transaction { amount, transaction_type: TransactionType::CashDeposit }
+    }
+
+    fn debit_of_amount(amount: usize) -> Self {
+        Transaction { amount, transaction_type: TransactionType::CashWithdrawal }
+    }
+}
+
 impl Account {
-    fn new() -> Self {
-        Account { balance: Cell::new(0) }
+    fn new(account_number: String) -> Self {
+        Account { account_number, balance: Cell::new(0) }
     }
 
-    pub fn credit(&self, amount: usize) {
+    pub fn credit(&self, transaction: Transaction) {
         let current_balance = self.balance.get();
-        self.balance.set(current_balance + amount)
+        self.balance.set(current_balance + transaction.amount)
     }
 
-    pub fn debit(&self, amount: usize) -> Result<(), String> {
+    pub fn debit(&self, transaction: Transaction) -> Result<(), String> {
         let current_balance = self.balance.get();
-        if current_balance >= amount {
-            self.balance.set(current_balance - amount);
+        if current_balance >= transaction.amount {
+            self.balance.set(current_balance - transaction.amount);
             Ok(())
         } else {
             Err("Insufficient balance".to_string())
@@ -37,24 +58,24 @@ mod tests {
 
     #[test]
     fn should_credit_in_account() {
-        let account = Account::new();
-        account.credit(1000.45 as usize);
+        let account = Account::new("abcd1234".to_string());
+        account.credit(Transaction::credit_of_amount(1000.45 as usize));
         assert_eq!(account.get_balance(), 1000.45 as usize)
     }
 
     #[test]
     fn should_debit_from_account() {
-        let account = Account::new();
-        account.credit(1000.45 as usize);
-        account.debit(100 as usize).unwrap();
+        let account = Account::new("abcd1234".to_string());
+        account.credit(Transaction::credit_of_amount(1000.45 as usize));
+        account.debit(Transaction::debit_of_amount(100 as usize)).unwrap();
         assert_eq!(account.get_balance(), 900.45 as usize)
     }
 
     #[test]
     fn should_give_error_for_debit_on_insufficient_balance() {
-        let account = Account::new();
-        account.credit(1000.45 as usize);
-        let error = account.debit(2000 as usize);
+        let account = Account::new("abcd1234".to_string());
+        account.credit(Transaction::credit_of_amount(1000.45 as usize));
+        let error = account.debit(Transaction::debit_of_amount(2000 as usize));
 
         assert_eq!(error, Err("Insufficient balance".to_string()));
     }
