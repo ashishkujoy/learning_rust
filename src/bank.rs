9e9c57c95d86af8bb1400aeb2bc914c3be69fdb8
Ashish Kumar <ashishku@thoughtworks.com>
2020-09-12 08:44:49 +0530

    Adds a method in bank to withdraw cash from an account

--- src/bank.rs
+++ src/bank.rs
@@ -29,6 +29,12 @@ impl Bank {
         })).unwrap_or(Err("Account not found".to_string()))
     }
 
+    pub fn withdraw_cash(&self, account_number: &str, amount: usize) -> Result<(), String> {
+        self.map_account(account_number, Box::new(move |account| {
+            account.perform_transaction(Transaction::debit_of_amount(amount))
+        })).unwrap_or(Err("Account not found".to_string()))
+    }
+
     pub fn check_balance(&self, account_number: &str) -> Option<usize> {
         self.map_account(account_number, Box::new(|account| { account.get_balance() }))
     }
@@ -72,5 +78,40 @@ mod tests {
 
         bank.deposit_cash(&account_number, 1400 as usize);
         assert_eq!(bank.check_balance(&account_number), Some(1400 as usize));
+    }
+
+    #[test]
+    fn should_debit_cash_from_given_account() {
+        let bank = Bank::new();
+        let account_number = bank.create_account();
+
+        bank.deposit_cash(&account_number, 1400 as usize);
+        bank.withdraw_cash(&account_number, 400 as usize);
+        assert_eq!(bank.check_balance(&account_number), Some(1000 as usize));
+    }
+
+    #[test]
+    fn should_give_error_when_account_has_insufficient_balance_for_debit() {
+        let bank = Bank::new();
+        let account_number = bank.create_account();
+
+        bank.deposit_cash(&account_number, 1400 as usize);
+        assert!(bank.withdraw_cash(&account_number, 2000 as usize).is_err())
+
+    }
+
+    #[test]
+    fn should_give_error_when_account_does_not_exists() {
+        let bank = Bank::new();
+
+        assert_eq!(bank.withdraw_cash("0123", 400 as usize), Err("Account not found".to_string()));
+        assert_eq!(bank.deposit_cash("0123", 400 as usize), Err("Account not found".to_string()));
+    }
+
+    #[test]
+    fn should_given_none_when_account_does_not_exists() {
+        let bank = Bank::new();
+
+        assert!(bank.check_balance("0123").is_none())
     }
 }
